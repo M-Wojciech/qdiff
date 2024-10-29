df359a7bd8b380cb44bdf3ba837ba1c25ed8a46d
Wojciech Mularczyk <wj.mularczyk@gmail.com>
2024-10-29 03:45:43 +0100

    libgit error check moved to function

--- main.c
+++ main.c
@@ -6,6 +6,14 @@ int is_file_in_repository(const char *filepath) {
     return 1;
 }
 
+void libgit_error_check(int error){
+    if (error < 0) {
+    const git_error *e = git_error_last();
+    printf("Error %d/%d: %s\n", error, e->klass, e->message);
+    exit(error);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2){
@@ -17,12 +25,8 @@ int main(int argc, char *argv[])
     git_libgit2_init();
     git_repository *repo = NULL;
     int error = git_repository_open_ext(&repo, filename, 0, NULL);
+    libgit_error_check(error);
 
-    if (error < 0) {
-    const git_error *e = git_error_last();
-    printf("Error %d/%d: %s\n", error, e->klass, e->message);
-    exit(error);
-    }
 
 
     git_repository_free(repo);
